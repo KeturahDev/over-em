4aab714cd4bfa00f638706e1acab7e2876d1402f
Keturah Howard <keturah.dev@gmail.com>
2021-04-24 16:01:34 -0700

    fill in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styles from 'styled-components'
+
+const MyButton = styles.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 1em 1em;
+  padding: 0.25em 1em;
+`
 
 const Header = () => {
   return (
     <React.Fragment>
-      <h1>Title</h1>
+      <h1>Over Em</h1>
+      <p>A place to let go of your ex.</p>
+      <button>why</button>
+      <button>how</button>
+
     </React.Fragment>
   )
 }
